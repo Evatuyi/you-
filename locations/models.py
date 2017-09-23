e716cdcdaddc8adc649b39c9446fda9bee9604a8
BilieuR <bilieureath1@gmail>
2017-09-23 14:47:20 +1000

    Added Adding Locations mockup and Bootstrap CDN

--- locations/models.py
+++ locations/models.py
@@ -10,3 +10,6 @@ class Location(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_at = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title
--- locations/views.py
+++ locations/views.py
@@ -1,11 +1,37 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Location
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('Hello World')
+    locations = Location.objects.all()[:10]
+    context = {
+        'locations': locations
+    }
+    return render(request, 'index.html', context)
+
+
+def details(request, id):
+    location = Location.objects.get(id=id)
+    context = {
+        'location': location
+    }
+    return render(request, 'details.html', context)
+
+
+def add(request):
+    if(request.method == 'POST'):
+        title = request.POST['title']
+        text = request.POST['text']
+
+        location = Location(title=title, text=text)
+        location.save()
+
+        return redirect('/locations')
+    else:
+        return render(request, 'add.html')
